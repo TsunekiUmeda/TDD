98bbf48dad8a444e9a3433417693c12d582f0163
Tsuneki Umeda <umedat@amazon.co.jp>
2021-11-24 20:44:52 +0900

    Chapter 15

--- src/money/Money.java
+++ src/money/Money.java
@@ -19,7 +19,7 @@ public class Money implements Expression {
                 && currency().equals((money.currency()));
     }
 
-    Money times(int multiplier) {
+    Expression times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
 
@@ -27,7 +27,7 @@ public class Money implements Expression {
         return currency;
     }
 
-    Expression plus(Money addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
